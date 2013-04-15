9717ac3adc586c53527277a937e39706d99b06b2
Maarten van Gompel <proycon@anaproy.nl>
2013-04-15 15:15:35 +0200

    client reads from stdin

--- client.py
+++ client.py
@@ -2,17 +2,21 @@
 
 import urllib
 import urllib2
+import sys
 
 URL = "http://localhost:8888"
 
 def client(tweettext):
     #data = urllib.urlencode({'input': tweettext})
-    headers = {"Content-type": "text/plain", "Accept": "text/plain"}
-    request = urllib2.Request(URL, "Dit is een test")
+    headers = {"Content-type": "application/json", "Accept": "application/json"}
+    request = urllib2.Request(URL, tweettext)
     response = urllib2.urlopen(request)
     return response.read()
 
-for i in range(0,1000000):
-    print i, client("Dit is een test")
+
+for i, line in enumerate(iter(sys.stdin.readline, '')):
+    line = line.strip()
+    print "Sent     #" +str(i) + ": \"" + line + "\""
+    print "Received #" + str(i) + ": \"" + client(line) + "\""
 
 
